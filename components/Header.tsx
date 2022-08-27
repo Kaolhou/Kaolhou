4ce1984a224972fcbe254c8e683320ed72a2638d
Kaolhou <xdroid42@gmail.com>
2022-08-27 16:40:08 -0300

    favicon is now in .jpg

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,16 +8,16 @@ export function Header({title,secondT}:headerProps){
     return(
         <Head>
             <meta name="og:title" content="Kaolhou" />
-            <meta property="og:image" content="https://kaolhou-dev.vercel.app/favicon.svg"/>
+            <meta property="og:image" content="https://kaolhou-dev.vercel.app/favicon.jpg"/>
             <meta property="og:description" content="A Curriculum Website by André Mendes"/>
 
             <meta name="twitter:title" content="Kaolhou"/>
             <meta name="twitter:description" content="A Curriculum Website by André Mendes"/>
             <meta name="twitter:card" content="summary" />
             <meta name="twitter:creator" content="@kaolhou" />
-            <meta name="twitter:image" content="https://kaolhou-dev.vercel.app/favicon.svg"/>
+            <meta name="twitter:image" content="https://kaolhou-dev.vercel.app/favicon.jpg"/>
             <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-            <link rel="apple-touch-icon" href="https://kaolhou-dev.vercel.app/favicon.svg" />
+            <link rel="apple-touch-icon" href="https://kaolhou-dev.vercel.app/favicon.jpg" />
             <title>{title||secondT||'Kaolhou'}</title>
 
         </Head>
